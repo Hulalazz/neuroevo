429933086512bbd7cd1b8ea4e0306de213aef548
Giuseppe Cuccu <giuseppe.cuccu@gmail.com>
2016-04-06 15:44:20 +0200

    Found another bug in NMatrix :(

--- spec/monkey_spec.rb
+++ spec/monkey_spec.rb
@@ -189,8 +189,9 @@ describe :monkey do
 end
 
 describe NMatrix, :skip do
+
   # method #hconcat doesn't work! => wrote hjoin (and vjoin)
-  context "#concat" do
+  describe "#concat" do
     it "should work with smaller matrices" do
       a = NMatrix.new([1,3], [1,2,3])
       b = NMatrix.new([1,2], [4,5])
@@ -202,8 +203,9 @@ describe NMatrix, :skip do
       expect(a.concat(b)).to eq(NMatrix.new([1,7], [1,2,3,4,5,6,7]))
     end
   end
-  # method #to_a doesn't work! => wrote true_to_a (fixing it breaks #new)
-  context "#to_a" do
+
+  # method #to_a not consistent! => wrote true_to_a (fixing it breaks #new)
+  describe "#to_a" do
     it "should always return an array with the same shape as the matrix" do
       { [2,2] => [[1,2],[3,4]],        # square
         [2,3] => [[1,2,3],[4,5,6]],    # rectangular (h)
@@ -214,6 +216,46 @@ describe NMatrix, :skip do
       }.each do |shape, ary|
         expect(NMatrix.new(shape, ary.flatten).to_a).to eq ary
       end
+    end
+  end
+
+  # method #[] works with ranges only sometimes!
+  # case to reproduce: single-row matrix
+  describe "#[]" do
+    context "with single value on the right" do
+      it "should work consistently with ranges" do
+        mat = NMatrix.zeros(3)
+        assert mat == NMatrix[[0,0,0],[0,0,0],[0,0,0]]
+        mat[0,0] = 1
+        assert mat == NMatrix[[1,0,0],[0,0,0],[0,0,0]]
+        mat[0..1,0..1] = 1
+        assert mat == NMatrix[[1,1,0],[1,1,0],[0,0,0]]
+        mat[0..1,0..-1] = 1
+        assert mat == NMatrix[[1,1,1],[1,1,1],[0,0,0]]
+        mat[0..-1,0..1] = 1
+        assert mat == NMatrix[[1,1,1],[1,1,1],[1,1,0]]
+        mat[0..-1,0..-1] = 1
+        assert mat == NMatrix[[1,1,1],[1,1,1],[1,1,1]]
+      end
+      it "should work with negative indices"
+    end
+
+    context "with array of values on the right" do
+      it "should work consistently with ranges" do
+        mat = NMatrix.zeros(3)
+        assert mat == NMatrix[[0,0,0],[0,0,0],[0,0,0]]
+        mat[0,0] = [1]
+        assert mat == NMatrix[[1,0,0],[0,0,0],[0,0,0]]
+        mat[0..1,0..1] = [1]*4
+        assert mat == NMatrix[[1,1,0],[1,1,0],[0,0,0]]
+        mat[0..1,0..-1] = [1]*6
+        assert mat == NMatrix[[1,1,1],[1,1,1],[0,0,0]]
+        mat[0..-1,0..1] = [1]*8
+        assert mat == NMatrix[[1,1,1],[1,1,1],[1,1,0]]
+        mat[0..-1,0..-1] = [1]*9
+        assert mat == NMatrix[[1,1,1],[1,1,1],[1,1,1]]
+      end
+      it "should work with negative indices"
     end
   end
 end
