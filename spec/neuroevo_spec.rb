0ce7c5e5e2876f8a298135180999f8b88e44ad81
Giuseppe Cuccu <giuseppe.cuccu@gmail.com>
2016-04-06 18:30:06 +0200

    Fitness reorganization for better visibility. Still not working.
    Uhm, doesn't work even with the right weights... Bug is down there!

--- spec/neuroevo_spec.rb
+++ spec/neuroevo_spec.rb
@@ -12,18 +12,31 @@ describe "Neuroevolution" do
       net = FFNN.new [2,2,1], act_fn: :logistic
       xor_err = lambda do |weights|
         net.load_weights weights
-        xor_table.inject(0) do |mem, (input, target)|
-          mem + target - net.activate(input).first
+        acts = {} # more readable than inject - we're testing here!
+        xor_table.each do |input, _|
+          acts[input] = net.activate(input).first
         end
+        res = {}
+        acts.each do |input, trg|
+          res[input] = trg>0.5 == xor_table[input]>0.5 # do they agree?
+        end
+        require 'pry'; binding.pry if res.values.all?
+        errs = acts.collect { |k,v| xor_table[k] - v}
+        errs.reduce :+
       end
 
       context "using XNES as optimizer" do
         fit = lambda { |inds| inds.collect { |ind| xor_err.call ind } }
         nes = XNES.new net.nweights, fit, :min
 
+        it "the fitness is correct",:focus do
+          solution_weights = [ [[1,2],[1,2],[0,0]],  [[-1],[8.5],[0]] ]
+          res = fit.([solution_weights.flatten]).first
+          assert res.approximates? 0
+        end
+
         it "correctly approximates XOR" do
-          nes.run ntrain: 50, printevery: false
-          # puts "  weights: #{net.layers}"
+          nes.run ntrain: 5000, printevery: 100
           assert net.out.all? {|v| v.approximates? 0}
         end
       end
